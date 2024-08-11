10cf769c8135a2ebe61ac215c5808b7710086bed
kael <kaelscott8@gmail.com>
2024-08-11 18:47:44 -0300

    pilha estatica

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PilhaEstatica<Integer> pilha = new PilhaEstatica<>(5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        pilha.push(10);
+        pilha.push(20);
+        pilha.push(30);
+
+        System.out.println(pilha.pop()); // 30
+        System.out.println(pilha.pop()); // 20
+
+        pilha.clear();
+
+        System.out.println(pilha.isEmpty()); // true
     }
 }
